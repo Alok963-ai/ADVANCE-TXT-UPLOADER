80422d87af61ccac9b4b7da2e9b4fb94d21c8daf
Alok963-ai <alokjangde6@gmail.com>
2025-05-15 14:16:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 7833842279 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [7833842279]
